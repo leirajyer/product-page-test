64aa33f1bd51a0c3e681dc8add9f502e8693c737
leirajyer <reylesterjariel@gmail.com>
2023-11-30 16:06:31 +0800

    first commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Product Page</title>
 
-        <script defer src="{{ mix('js/app.js') }}"></script>
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    </head>
-    <body class="antialiased">
-        <div id="app">
-            <router-view></router-view>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Product Page</title>
+
+    <script defer src="{{ mix('js/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+
+<body class="antialiased">
+    <div id="app">
+        <div>
+            <h1>{{ $product['name'] }}</h1>
+
+            <p>{{ $product['description'] }}</p>
+
+            <p>Price: ${{ $product['price']['discounted'] }}</p>
+
+            <p>Discount: {{ $product['discount']['amount'] }}% off</p>
+            <a href="" class="btn">sample</a>
+            <h2>Images</h2>
+            <ul>
+                @foreach ($product['images'] as $image)
+                    <li><img src="{{ $image }}" alt="Product Image"></li>
+                @endforeach
+            </ul>
         </div>
-    </body>
+        <hr>
+
+    </div>
+</body>
+
 </html>
